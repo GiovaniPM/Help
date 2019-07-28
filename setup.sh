9777d0b5290e0559528c8b63389fd1116ef10ced
Giovani Perotto Mesquita <giovanipm@gmail.com>
2019-07-28 19:45:43 -0300

    giova(DESKTOP-CUMM31F) 190728194543

--- setup.sh
+++ setup.sh
@@ -2,33 +2,39 @@
 #=====================================================================================================
 #= Created by Giovani Perotto Mesquita                                                               =
 #=====================================================================================================
-echo -e '+-\033[41;30;4m Setup: \033[m-----------------------------------------------------------------------------------------------------------+'
-echo -e '|                                                                                                                    |'
-echo -e '|    Setup directory                                                                                                 |'
-echo -e '|                                                                                                                    |'
-echo -e '+--------------------------------------------------------------------------------------------------------------------+'
+echo -e '+-\033[41;30;4m Setup: \033[m----------------------------------------------------------------+'
+echo -e '|                                                                         |'
+echo -e '|    Setup directory                                                      |'
+echo -e '|                                                                         |'
+echo -e '+-------------------------------------------------------------------------+'
 
-set gitdir=Help
-echo -e "$IYellow $gitdir $Color_Off"
+export gitdir="Help"
+export gitlink="https://github.com/GiovaniPM/$gitdir.git"
+export IYellow="\033[0;93m"
+export Color_Off="\033[0m"
+export IGreen="\033[0;92m"
+export On_Green="\033[42m"
 
-if [ -d "../"$gitdir]; then
-  echo -e "$IYellow Getting code $Color_Off"
+echo -e "->$IYellow $gitlink $Color_Off"
+
+if [ -d "../$gitdir" ]; then
+  echo -e "->$IYellow Getting code actual directory $Color_Off"
   git pull
   cd ..
 else
   if [ -d "./$gitdir" ]; then
-    echo -e "$IYellow Getting code $Color_Off"
+    echo -e "->$IYellow Getting code $Color_Off"
     cd $gitdir
     git pull
     cd ..
   else
     echo -e "$IYellow Cloning $Color_Off"
-    git clone https://github.com/GiovaniPM/$gitdir.git Help
-    echo -e "$IYellow Getting code $Color_Off"
+    git clone $gitlink Help
+    echo -e "->$IYellow Getting code $Color_Off"
     cd $gitdir
     git pull
     cd ..
   fi
 fi
 
-echo -e "$IGreen Finished $Color_Off"
+echo -e "->$On_Green Finished $Color_Off"
