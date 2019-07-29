4ab3b05027bf936357866e237cb46948444917d6
Giovani Mesquita <giovani.mesquita@grupobs.com.br>
2019-07-29 16:22:08 -0300

    Giovani_Mesquita(RBST7497) 190729162207

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 #=====================================================================================================
 #= Created by Giovani Perotto Mesquita                                                               =
 #=====================================================================================================
-echo -e '\033[1;49;34m+- Setup: ----------------------------------------------------------------+\033[m'
+echo -e '\033[1;49;34m+-\033[0;106;97m Setup: \033[1;49;34m----------------------------------------------------------------+\033[m'
 echo -e '\033[1;49;34m|                                                                         |\033[m'
 echo -e '\033[1;49;34m|\033[m    Download repository                                                  \033[1;49;34m|\033[m'
 echo -e '\033[1;49;34m|                                                                         |\033[m'
@@ -16,22 +16,28 @@ fi
 
 export gitlink="https://github.com/GiovaniPM/$gitdir.git"
 
-echo -e "->\033[0;93m $gitlink \033[0m"
+echo -e "->\033[0;106;97m $gitlink \033[m"
 
 if [ -d "../$gitdir" ]; then
-  echo -e "->\033[0;93m Getting code actual directory \033[0m"
+  echo -e "->\033[0;103;30m Getting code actual directory \033[m"
+  echo -e "\033[0;49;92m"
   git pull
+  echo -e "\033[m"
   cd ..
 else
   if [ -d "./$gitdir" ]; then
-    echo -e "->\033[0;93m Getting code \033[0m"
+    echo -e "->\033[0;103;30m Getting code \033[m"
     cd $gitdir
+	echo -e "\033[0;49;92m"
     git pull
+	echo -e "\033[m"
     cd ..
   else
-    echo -e "->\033[0;93m Cloning \033[0m"
+    echo -e "->\033[0;103;30m Cloning \033[m"
+	echo -e "\033[0;49;92m"
     git clone $gitlink $gitdir
+	echo -e "\033[m"
   fi
 fi
 
-echo -e "->\033[42m Finished \033[0m"
+echo -e "->\033[0;106;97m Finished \033[m"
