277e143c9217ee03d8cf23aceb852e5ec5733a62
Giovani Perotto Mesquita <giovanipm@gmail.com>
2019-12-02 15:03:05 -0300

    Giovani_Mesquita(RBST10164) 191202150304

--- scripts/git/gitbkp.sh
+++ scripts/git/gitbkp.sh
@@ -8,12 +8,17 @@ export IGreen="\033[0;92m"
 export On_Green="\033[42m"
 export backdir=$(pwd)
 
+DIR="BKP"
+if [ ! -d "$DIR" ]; then
+  mkdir "$DIR"
+fi
+
 for directory in $(ls -a)
 do
   if [ "$directory" != "." ] &&  [ "$directory" != ".." ] &&  [ "$directory" != "BKP" ]; then
     echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
     echo -e $IGreen"  Directory: $directory"$Color_Off
     echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
-    tar czvf "./BKP/$directory.gz" "./$directory"
+    tar --exclude=".git" -czvf ./$DIR/$directory`date +%y%m%d%H%M%S`.gz ./$directory/**
   fi
 done
