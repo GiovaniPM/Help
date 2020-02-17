8afee200cb12f315ef4f42a9cfc0d2fa17f5b441
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-02-16 22:05:41 -0300

    giova(DESKTOP-PG18CKU) 200216220541

---
+++ scripts/chocolatey/DESKTOP-PG18CKU.bat
@@ -0,0 +1,36 @@
+@echo off 
+REM =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+REM DESKTOP-PG18CKU 
+REM Capturado em: 16/02/2020 22:04 
+REM =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= 
+choco install Chocolatey -y 
+choco install boxes.portable -y 
+choco install busybox -y 
+choco install chocolatey -y 
+choco install chocolatey-core.extension -y 
+choco install chocolatey-windowsupdate.extension -y 
+choco install ConEmu -y 
+choco install curl -y 
+choco install docker -y 
+choco install docker-cli -y 
+choco install docker-desktop -y 
+choco install DotNet4.5.2 -y 
+choco install git -y 
+choco install git.install -y 
+choco install jq -y 
+choco install KB2919355 -y 
+choco install KB2919442 -y 
+choco install KB2999226 -y 
+choco install KB3033929 -y 
+choco install KB3035131 -y 
+choco install mRemoteNG -y 
+choco install python2 -y 
+choco install python3 -y 
+choco install vcredist140 -y 
+choco install vcredist2015 -y 
+choco install vscode -y 
+choco install vscode.install -y 
+choco install Wget -y 
+choco install Did -y 
+choco install Features? -y 
+choco install https://chocolatey.org/compare -y 
