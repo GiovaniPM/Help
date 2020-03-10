3322d7ee8d367578b8708059761b37b65ca8b896
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-03-10 11:31:38 -0300

    Giovani_Mesquita(RBST10164) 200310113137

---
+++ scripts/dos/Cereal.bat
@@ -0,0 +1,10 @@
+@echo off
+title Alerta
+
+echo result=Msgbox("Coma uma barrinha de cereal!",vbOk,"Lembrete") > %TEMP%\~input.vbs
+echo WScript.Echo result >> %TEMP%\~input.vbs
+
+cscript //nologo %TEMP%\~input.vbs
+
+::del %TEMP%\~input.vbs
+type %TEMP%\~input.vbs
