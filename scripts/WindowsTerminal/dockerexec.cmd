64a81fdfd58406c9d6a523bb2e09071fd2e67bf1
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-06-08 18:49:29 -0300

    giova(DESKTOP-PG18CKU) 200608184929

--- scripts/WindowsTerminal/dockerexec.cmd
+++ scripts/WindowsTerminal/dockerexec.cmd
@@ -1,10 +1,18 @@
 @echo off
 
+:Menu
 cls
 ::docker image ls
 docker ps
-echo Entre com a IMAGE
+echo Entre com a IMAGE (deixe branco para sair)
 set /p Nome=
+
+if [%Nome%]==[] goto Sair
+
 title Docker - %Nome%
 docker exec -it %Nome% bash
-exit
+set Nome=
+goto Menu
+
+:Sair
+exit /b
