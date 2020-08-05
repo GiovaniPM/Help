a8e529c169985108bd25a4ff5515f6275f99a136
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-08-05 19:26:01 -0300

    giova(DESKTOP-PG18CKU) 200805192600

--- scripts/dos/Cereal.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Alerta
-
-echo result=Msgbox("Coma uma barrinha de cereal!",vbOk,"Lembrete") > %TEMP%\~input.vbs
-echo WScript.Echo result >> %TEMP%\~input.vbs
-
-cscript //nologo %TEMP%\~input.vbs
-
-del %TEMP%\~input.vbs
--- scripts/dos/montadrv.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-mount C C:\Users\giova\projetos\Help\scripts\dos
