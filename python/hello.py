a1a456f6a619ed71806e1099a0ce4a440db91e6e
Giovani Perotto Mesquita <giovanipm@gmail.com>
2020-12-18 21:06:03 -0300

    giova(DESKTOP-PG18CKU) 201218210603

---
+++ python/hello.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', defaults={'name': 'Flask'})
+@app.route('/<name>')
+def index(name):
+    return render_template('hello.html',name=name)
+
+if __name__ == '__main__':
+    app.run()
