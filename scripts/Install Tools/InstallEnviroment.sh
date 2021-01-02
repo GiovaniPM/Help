c258317ebc2334add4b035bc93d0e3f7ef24e9c2
Giovani Perotto Mesquita <giovanipm@gmail.com>
2021-01-02 18:41:12 -0300

    giova(DESKTOP-PG18CKU) 210102184112

--- scripts/Install Tools/InstallEnviroment.sh
+++ scripts/Install Tools/InstallEnviroment.sh
@@ -17,12 +17,12 @@ then
     chmod 777 $PROJDIR/*.sh
 fi
 
-if ! [ -x "$(command -v choco)" ]; then
-  scriptchoco = "'https://chocolatey.org/install.ps1'"
+if [ -x "$(command -v choco)" ]; then
+  scriptchoco="'https://chocolatey.org/install.ps1'"
   echo 'Error: choco is not installed.'
   echo '  Run the follow line:'
   #echo '  - "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'
-  echo '  - "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString("$scriptchoco"))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'
+  echo '  - "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('${scriptchoco}'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'
   exit
 fi
 
