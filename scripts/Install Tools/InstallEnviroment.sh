15686aff6edc5333c639faba091a28fe8cb9d9f1
Giovani Perotto Mesquita <giovanipm@gmail.com>
2021-01-02 18:36:26 -0300

    giova(DESKTOP-PG18CKU) 210102183625

--- scripts/Install Tools/InstallEnviroment.sh
+++ scripts/Install Tools/InstallEnviroment.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
-PROJDIR="Projetos"
-HELPDIR="Help"
+BASEDIR=`echo ~`
+PROJDIR=$BASEDIR/"Projetos"
+HELPDIR=$PROJDIR/"Help"
 GITDIR=$HELPDIR"/scripts/git"
 
-if [ ! -d "${PROJDIR}" ]
+if ! [ -d "${PROJDIR}" ]
 then
     mkdir $PROJDIR
 fi
 
-cd ${PROJDIR}
-
-if [ ! -d "${HELPDIR}" ]
+if ! [ -d "${HELPDIR}" ]
 then
+    cd ${PROJDIR}
     git clone https://github.com/GiovaniPM/Help.git
+    cp $GITDIR/*.sh $PROJDIR/.
+    chmod 777 $PROJDIR/*.sh
 fi
 
-cp $GITDIR/* ../.
-chmod 777 ../*.sh
+if ! [ -x "$(command -v choco)" ]; then
+  scriptchoco = "'https://chocolatey.org/install.ps1'"
+  echo 'Error: choco is not installed.'
+  echo '  Run the follow line:'
+  #echo '  - "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'
+  echo '  - "%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString("$scriptchoco"))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"'
+  exit
+fi
+
+if ! [ -x "$(command -v code)" ]; then
+  echo 'Error: vscode is not installed.'
+  choco install vscode -y
+fi
+
+echo "BASEDIR="$BASEDIR
+echo "PROJDIR="$PROJDIR
+echo "HELPDIR="$HELPDIR
+echo "GITDIR="$GITDIR
