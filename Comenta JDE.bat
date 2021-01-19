e75614e829ed52eb6c83a8710047c0b10ddc321a
Giovani Perotto Mesquita <giovanipm@gmail.com>
2021-01-19 16:24:05 -0300

    giova(DESKTOP-PG18CKU) 210119162405

---
+++ Comenta JDE.bat
@@ -0,0 +1,13 @@
+@echo off
+set SPACES=...............................................................................
+set HEADER_COM=%USERNAME%/%COMPUTERNAME% - %DATE% %TIME% - Start %SPACES%
+set TRAIL_COM=%USERNAME%/%COMPUTERNAME% - %DATE% %TIME% - End %SPACES%
+cls
+echo %HEADER_COM:~0,66% > temporario.txt
+echo ------------------------------------------------------------------- >> temporario.txt
+echo Notes: >> temporario.txt
+notepad temporario.txt
+echo . >> temporario.txt
+type temporario.txt | boxes -d jde
+echo %TRAIL_COM:~0,66% | boxes -d jde
+del temporario.txt
