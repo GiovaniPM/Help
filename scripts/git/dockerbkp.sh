7c6b45de5d4b735eae32a8bea45a086cae0eb856
Giovani <giovanipm@gmail.com>
2022-03-12 12:36:12 -0300

    giova(DESKTOP-PG18CKU) 220312123612

---
+++ scripts/git/dockerbkp.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+declare -a OPT_ARRAY
+
+export IYellow="\033[0;93m"
+export Color_Off="\033[0m"
+export IGreen="\033[0;92m"
+export On_Green="\033[42m"
+export Black="\033[0;30m"
+re='^[0-9]+$'
+
+#docker ps -a --format "table{{.ID}} {{.Image}}" > ./temp.txt
+#docker images --format "table{{.Repository}}" > ./temp.txt
+#docker images --format "{{.Repository}}" > ./temp.txt
+
+i=0
+for t in $(docker images --format "{{.Repository}}" | sort); do
+    MEU_ARRAY[$i]=$t
+    let i=i+1
+done
+
+clear
+echo -e '============================================================================='
+echo -e 'Option Image                          | Option Image                         '
+echo -e '====== ============================== + ====== =============================='
+
+linstr=''
+srtpar=0
+i=1
+for t in ${MEU_ARRAY[*]}; do
+    spaces=$(printf '%0.1s' '.'{1..10})
+    numstr=$i$spaces
+    numstr=${numstr:0:6}
+    spaces=$(printf '%0.1s' '.'{1..30})
+    optstr=$t$spaces
+    optstr=${optstr:0:30}
+    string=$(echo -e $IYellow$numstr$Color_Off' '$IGreen$optstr$Color_Off)
+    if [[ $strpar -eq 0 ]]; then
+        linstr=$string
+        strpar=1
+    else
+        linstr=$linstr' | '$string
+        echo $linstr
+        linstr=''
+        strpar=0
+    fi
+    let i=i+1
+done
+if [[ $strpar -eq 1 ]]; then
+    echo $linstr
+fi
+
+echo -e '============================================================================='
+echo -e 'Select the option to backup ('0' backup all images): \c'
+read option
+
+clear
+
+i=1
+for t in ${MEU_ARRAY[*]}; do
+    if [[ $option =~ $re ]] ; then
+        if [ $i -eq $option -o $option -eq 0 ]; then
+            filename=$t
+            imagename=$t
+            textfrom="/"
+            textto="_"
+            filename="${filename//$textfrom/$textto}"
+            textfrom="."
+            textto="_"
+            filename="${filename//$textfrom/$textto}"
+            echo -e $imagename' --> '$filename.img'\c'
+            docker save -o $filename.img $imagename
+            echo -e '\a ('$On_Green'ok'$Color_Off')'
+        fi
+    fi
+    let i=i+1
+done
--- scripts/git/gitsync.sh
+++ scripts/git/gitsync.sh
@@ -7,7 +7,8 @@ export Color_Off="\033[0m"
 export IGreen="\033[0;92m"
 export On_Green="\033[42m"
 export backdir=$(pwd)
-export file=./upload.per
+export fileU=./upload.per
+export fileD=./download.per
 
 if [ "X$1" != "XD" ] && [ "X$1" != "Xd" ] && [ "X$1" != "XU" ] && [ "X$1" != "Xu" ]; then 
   echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
@@ -26,13 +27,19 @@ do
     echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
     if [ "X$1" == "XD" ] ||  [ "X$1" == "Xd" ]; then
       cd $directory
-      ~/download.sh
+      if test -f "$fileD"; then
+        ~/download.sh
+      else
+        echo "Bypassed by flag -> download.per"
+      fi
       cd $backdir
     fi
     if [ "X$1" == "XU" ] ||  [ "X$1" == "Xu" ]; then
       cd $directory
-      if test -f "$file"; then
+      if test -f "$fileU"; then
         ~/upload.sh
+      else
+        echo "Bypassed by flag -> upload.per"
       fi
       cd $backdir
     fi
