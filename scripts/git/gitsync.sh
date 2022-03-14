6282a8ab69785034be3e2e40c823a88a3577448b
Giovani <giovanipm@gmail.com>
2022-03-14 13:32:19 -0300

    giova(DESKTOP-PG18CKU) 220314133219

--- scripts/git/gitsync.sh
+++ scripts/git/gitsync.sh
@@ -5,6 +5,7 @@
 
 export Color_Off="\033[0m"
 export IGreen="\033[0;92m"
+export IYellow="\033[0;93m"
 export On_Green="\033[42m"
 export backdir=$(pwd)
 export fileU=./upload.per
@@ -13,12 +14,12 @@ export fileD=./download.per
 if [ "X$1" != "XD" ] && [ "X$1" != "Xd" ] && [ "X$1" != "XU" ] && [ "X$1" != "Xu" ]; then 
   echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
   echo -e $IGreen"  Usage: $directory"$Color_Off
-  echo -e $IGreen"    gitsync D - $On_Green Download directories $directory"$Color_Off
-  echo -e $IGreen"    gitsync U - $On_Green Upload directories   $directory"$Color_Off
+  echo -e $IGreen"    $IYellow gitsync D - $On_Green Download directories $directory"$Color_Off
+  echo -e $IGreen"    $IYellow gitsync U - $On_Green Upload directories   $directory"$Color_Off
   echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
   echo -e $IGreen"  Tokens:"$Color_Off
-  echo -e $IGreen"    $fileU   $On_Green(Put this file inside the directory to denie upload)"$Color_Off
-  echo -e $IGreen"    $fileD $On_Green(Put this file inside the directory to denie download)"$Color_Off
+  echo -e $IGreen"    $IYellow $fileU   - $On_Green Put this file inside the directory to denie upload"$Color_Off
+  echo -e $IGreen"    $IYellow $fileD - $On_Green Put this file inside the directory to denie download"$Color_Off
   echo -e $IGreen"--------------------------------------------------------------------------------------"$Color_Off
   exit
 fi
