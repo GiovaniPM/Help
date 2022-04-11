6f76b40339d41d6b30aa88b1fc84536309a03241
Giovani <giovanipm@gmail.com>
2022-04-11 14:20:53 -0300

    giova(DESKTOP-PG18CKU) 220411142053

--- docker/nginx test/build.bat
+++ docker/nginx test/build.bat
@@ -1,1 +1,7 @@
-docker build --tag=imagem-site-nginx .
+docker rm --force home
+docker rmi homebasic
+docker build -t homebasic .
+docker run -d --name home -p 8081:80 homebasic
+docker network connect myNetwork home
+REM docker network create myNetwork
+REM docker network inspect myNetwork
