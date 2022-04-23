122140b219e425fe5f2966ede4e44efada481115
Giovani <giovanipm@gmail.com>
2022-04-22 22:37:48 -0300

    giova(DESKTOP-PG18CKU) 220422223748

--- docker/Oracle/build.bat
+++ docker/Oracle/build.bat
@@ -1,7 +1,7 @@
 docker rm --force oraclexe
 docker rmi oraclebasic
 docker build -t oraclebasic .
-docker run -d --name oraclexe -p 1541:1541 -p 1521:1521 -p 5500:5500 oraclebasic
+docker run -d --name oraclexe -p 1541:1541 -p 1521:1521 -p 5500:5500 -v dadosxe:/opt/oracle/oradata oraclebasic
 docker network connect myNetwork oraclexe
 REM docker network create myNetwork
 REM docker network inspect myNetwork
