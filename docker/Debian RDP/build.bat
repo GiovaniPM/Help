bfddbee39d477893326a9d32356467b3079c476b
Giovani <giovanipm@gmail.com>
2022-04-24 13:03:15 -0300

    giova(DESKTOP-PG18CKU) 220424130315

--- docker/Debian RDP/build.bat
+++ docker/Debian RDP/build.bat
@@ -1,2 +1,10 @@
-docker build --tag=debiangfx .
+docker rm --force devbasic
+docker rmi debiangfx
+docker build -t debiangfx .
+docker run -itd --name devbasic -p 3350:3350 -p 3391:3391 debiangfx
+docker network connect myNetwork devbasic
+REM docker network create myNetwork
+REM docker network inspect myNetworkShow
+
+rem docker build --tag=debiangfx .
 rem docker run -it --name="dgfx" -p 3350:3350 -p 3391:3391 debiangfx
