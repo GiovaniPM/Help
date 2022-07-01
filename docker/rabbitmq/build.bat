60375c694790d36b4953c00899bbc6e26f1616d7
Giovani <giovanipm@gmail.com>
2022-07-01 11:16:51 -0300

    giova(DESKTOP-PG18CKU) 220701111650

--- docker/rabbitmq/build.bat
+++ docker/rabbitmq/build.bat
@@ -1,7 +1,22 @@
-docker rm --force rabbitmq
-docker rmi rabbitmqbasic
-docker build -t rabbitmqbasic .
-docker run -d --name rabbitmq -p 15671:15671 rabbitmqbasic
-docker network connect myNetwork rabbitmq
-REM docker network create myNetwork
-REM docker network inspect myNetwork
+@echo off
+
+set NetworkName="myNetwork"
+set ContainerName="rabbitmq"
+set ImageName="rabbitmqimg"
+
+docker network inspect %NetworkName%> nul
+IF "%ERRORLEVEL%" == "0" (goto CreatedNET)
+docker network create %NetworkName%
+echo Network %NetworkName% created!
+goto CreateIMG
+
+:CreatedNET
+echo Network %NetworkName% found!
+goto CreateIMG
+
+:CreateIMG
+docker rm --force %ContainerName%
+docker rmi %ImageName%
+docker build -t %ImageName% .
+docker run -d --name %ContainerName% -p 15671:15671 %ImageName%
+docker network connect %NetworkName% %ContainerName%
