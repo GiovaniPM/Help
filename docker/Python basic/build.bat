dfb7da3ce49767c97c5bafcba9ba6d1b5cf96e5a
Giovani <giovanipm@gmail.com>
2022-07-01 11:37:35 -0300

    giova(DESKTOP-PG18CKU) 220701113734

--- docker/Python basic/build.bat
+++ docker/Python basic/build.bat
@@ -1,7 +1,22 @@
-docker rm --force pythonserver
-docker rmi pythonbasic
-docker build -t pythonbasic .
-docker run -d --name pythonserver -p 5000:8080 pythonbasic
-docker network connect myNetwork pythonserver
-REM docker network create myNetwork
-REM docker network inspect myNetwork
+@echo off
+
+set NetworkName="myNetwork"
+set ContainerName="pythonserver"
+set ImageName="pythonbasic"
+
+docker network inspect %NetworkName%> nul
+IF "%ERRORLEVEL%" == "0" (goto CreatedNET)
+docker network create %NetworkName%
+echo Network %NetworkName% created!
+goto CreateIMG
+
+:CreatedNET
+echo Network %NetworkName% found!
+goto CreateIMG
+
+:CreateIMG
+docker rm --force %ContainerName%
+docker rmi %ImageName%
+docker build -t %ImageName% .
+docker run -d --name %ContainerName% -p 5000:8080 %ImageName%
+docker network connect %NetworkName% %ContainerName%
