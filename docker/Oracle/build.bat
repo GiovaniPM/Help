a046372d2cd952d3b0b5c8ed1be727c995702923
Giovani <giovanipm@gmail.com>
2022-10-04 15:43:34 -0300

    giova(DESKTOP-PG18CKU) 221004154333

--- docker/Oracle/build.bat
+++ docker/Oracle/build.bat
@@ -1,7 +1,36 @@
-docker rm --force oraclexe
-docker rmi oraclebasic
-docker build -t oraclebasic .
-docker run -d --name oraclexe -p 1541:1541 -p 1521:1521 -p 5500:5500 -v dadosxe:/opt/oracle/oradata oraclebasic
-docker network connect myNetwork oraclexe
+@echo off
+
+FOR /F "tokens=1,2,3,4 delims=/ " %%a IN ("%date%") DO set DateRun=%%c%%b%%a
+FOR /F "tokens=1,2,3,4 delims=: " %%a IN ("%time%") DO set TimeRun=%%a%%b%%c%%d
+
+set NetworkName="myNetwork"
+set ContainerName="oraclexe"
+set ImageName="oraclebasic"
+set PortList=-p 1541:1541 -p 1521:1521 -p 5500:5500
+set Volumes=-v dadosxe:/opt/oracle/oradata
+
+docker network inspect %NetworkName%> nul
+IF "%ERRORLEVEL%" == "0" (goto CreatedNET)
+docker network create %NetworkName%
+echo Network %NetworkName% created!
+goto CreateIMG
+
+:CreatedNET
+echo Network %NetworkName% found!
+goto CreateIMG
+
+:CreateIMG
+docker rm --force %ContainerName%
+docker rmi %ImageName%
+docker build -t %ImageName% .
+docker run -d --name %ContainerName% %PortList% -e BUILDED=%DateRun%%TimeRun% %Volumes% %ImageName%
+docker network connect %NetworkName% %ContainerName%
+docker network inspect %NetworkName%
+
+REM docker rm --force oraclexe
+REM docker rmi oraclebasic
+REM docker build -t oraclebasic .
+REM docker run -d --name oraclexe -p 1541:1541 -p 1521:1521 -p 5500:5500 -v dadosxe:/opt/oracle/oradata oraclebasic
+REM docker network connect myNetwork oraclexe
 REM docker network create myNetwork
 REM docker network inspect myNetwork
