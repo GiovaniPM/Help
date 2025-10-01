9e38163a271dabf45e01b29d61753fa9f4ffbdeb
Giovani Perotto Mesquita <giovanipm@gmail.com>
2025-10-01 20:08:44 -0300

    giova(DESKTOP-MGAILV5) 251001200844

--- JIRA/processa.py
+++ JIRA/processa.py
@@ -19,10 +19,9 @@ ABA_PARAMETROS = "Parâmetros"
 
 # Carrega setup para conexão
 with open('setup.json', 'r', encoding='utf-8') as f:
-    json_str = json.load(f)
-    objeto = json.loads(json_str)
-    url = objeto['url']
-    headers = objeto['headers']
+    objeto = json.load(f)
+    jiralib.url = objeto['url']
+    jiralib.headers = objeto['headers']
 
 # --- 1. LEITURA DAS ABAS ---
 df_planilha = pd.read_excel(ARQUIVO, sheet_name=ABA_PLANILHA, engine="openpyxl")
@@ -49,113 +48,97 @@ colunas_desejadas = ["Projeto",
                      "Tarefa",
                      "Etapa",
                      "Horas",
-                     "Pontos"]
+                     "Pontos",
+                     "TicketE",
+                     "TicketS",
+                     "TicketT"]
 df = df_planilha[colunas_desejadas].copy()
 
-'''
-# Garante que 'Horas' é numérico
-df["Horas"] = pd.to_numeric(df["Horas"], errors="coerce")
-
-# Cria a coluna 'Pontos' conforme as faixas de 'Horas'
-def calcular_pontos(horas):
-    if pd.isna(horas):
-        return np.nan
-    if horas < 8:
-        return 0.5
-    elif horas < 24:
-        return 1
-    elif horas < 40:
-        return 2
-    elif horas < 60:
-        return 3
-    elif horas < 80:
-        return 5
-    elif horas < 100:
-        return 8
-    else:
-        return 13
-
-df["Pontos"] = df["Horas"].apply(calcular_pontos)
-'''
-
-# Cria a coluna 'Nome' no formato solicitado
-df["Request"] = df["Request"].fillna("")
-df["Tipo"] = df["Tipo"].fillna("")
-df["Épico"] = df["Épico"].fillna("")
-df["Estória"] = df["Estória"].fillna("")
-df["Nome Épico"] = "[" + df["Request"].astype(str) + "] - " + df["Épico"].astype(str)
+# Cria as colunas customizadas no formato solicitado
+df["Request"]      = df["Request"].fillna("")
+df["Tipo"]         = df["Tipo"].fillna("")
+df["Épico"]        = df["Épico"].fillna("")
+df["Estória"]      = df["Estória"].fillna("")
+df["TicketE"]      = df["TicketE"].fillna("0")
+df["TicketS"]      = df["TicketS"].fillna("0")
+df["TicketT"]      = df["TicketT"].fillna("0")
+df["Nome Épico"]   = "[" + df["Request"].astype(str) + "] - " + df["Épico"].astype(str)
 df["Nome Estória"] = "[" + df["Tipo"].astype(str) + "] - " + df["Estória"].astype(str)
 
 # --- 4. INTEGRAÇÃO COM JIRA ---
 Epicos_criados = []
 Estorias_criadas = []
-Tarefas_criadas = []
-last_epic_key = None
-last_story_key = None
 Ticket_Epico = []
 Ticket_Estoria = []
 Ticket_Task = []
+last_epic_key = None
+last_story_key = None
 
 for row in df.to_dict(orient='records'):
     
     if row["Épico"] not in Epicos_criados:
-        response = jiralib.create_epic(row["Projeto"],
-                                       row["Nome Épico"],
-                                       row["Nome Épico"],
-                                       row["Data"],
-                                       row["Nome Épico"],
-                                       row["Usuário Projeto"],
-                                       row["Task"],
-                                       row["Módulo"],
-                                       row["Tipo"]
-                                       )
-        # Extrai a chave do épico criado
-        valor_da_key = response.json().get("key")
-        
-        print(row["Nome Épico"])
-        print(response.json())
+        if row["TicketE"] == "0":
+            response = jiralib.create_epic(row["Projeto"],
+                                           row["Nome Épico"],
+                                           row["Nome Épico"],
+                                           row["Data"],
+                                           row["Nome Épico"],
+                                           row["Usuário Projeto"],
+                                           row["Task"],
+                                           row["Módulo"],
+                                           row["Tipo"]
+                                           )
+            # Extrai a chave do épico criado
+            valor_da_key = response.json().get("key")
+            
+            print(row["Nome Épico"])
+            print(response.json())
+        else:
+            valor_da_key = row["TicketE"]
         
         last_epic_key = valor_da_key
         
         Epicos_criados.append(row["Épico"])
     
-    if row["Estória"] not in Estorias_criadas:
-        response = jiralib.create_story(row["Projeto"],
-                                        "História",
-                                        row["Nome Estória"],
-                                        row["Nome Estória"],
-                                        row["UUID PO"],
-                                        row["UUID Recurso"],
-                                        last_epic_key,
-                                        row["Pontos"]
-                                        )  
-        # Extrai a chave da estória criada
-        valor_da_key = response.json().get("key")
-        
-        print("    ", row["Nome Estória"])
-        print("    ", response.json())
+    StoryKey = "[" + row["Task"] + "] - " + row["Estória"]
+    
+    if StoryKey not in Estorias_criadas:
+        if row["TicketS"] == "0":
+            response = jiralib.create_story(row["Projeto"],
+                                            "História",
+                                            row["Nome Estória"],
+                                            row["Nome Estória"],
+                                            row["UUID PO"],
+                                            row["UUID Recurso"],
+                                            last_epic_key,
+                                            row["Pontos"]
+                                            )  
+            # Extrai a chave da estória criada
+            valor_da_key = response.json().get("key")
+            
+            print("    ", row["Nome Estória"])
+            print("    ", response.json())
+        else:
+            valor_da_key = row["TicketS"]
         
         last_story_key = valor_da_key
         
-        Estorias_criadas.append(row["Estória"])
+        Estorias_criadas.append(StoryKey)
     
-    if row["Tarefa"] not in Tarefas_criadas:
-        response = jiralib.create_task(row["Projeto"],
-                                       row["Etapa"],
-                                       row["Tarefa"],
-                                       row["Tarefa"],
-                                       row["UUID Recurso"],
-                                       row["UUID Recurso"],
-                                       last_story_key,
-                                       row["Horas"] + "h"
-                                       )
-        # Extrai a chave da Tarefa criada
-        valor_da_key = response.json().get("key")
-        
-        print("        ", row["Tarefa"])
-        print("        ", response.json())
-        
-        Tarefas_criadas.append(row["Tarefa"])
+    response = jiralib.create_task(row["Projeto"],
+                                   row["Etapa"],
+                                   row["Tarefa"],
+                                   row["Tarefa"],
+                                   row["UUID Recurso"],
+                                   row["UUID Recurso"],
+                                   last_story_key,
+                                   str(row["Horas"]) + "h"
+                                   )
+    # Extrai a chave da Tarefa criada
+    valor_da_key = response.json().get("key")
+    
+    print("        ", row["Tarefa"])
+    print("        ", response.json())
     
     Ticket_Epico.append(last_epic_key)
     Ticket_Estoria.append(last_story_key)
