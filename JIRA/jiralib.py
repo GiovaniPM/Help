3cc7db357c9fa770964a8c4e934d7dd9b34b5a64
Giovani Perotto Mesquita <giovanipm@gmail.com>
2025-10-10 16:47:08 -0300

    Update jiralib.py

--- JIRA/jiralib.py
+++ JIRA/jiralib.py
@@ -47,13 +47,13 @@ def create_epic(project, summary, description, due_date, epic_name, reporter_ema
     "fields": {}
   }
 
+  payloadObj["fields"]["customfield_10018"] = epic_name
   payloadObj["fields"]["customfield_10101"] = reporter_email
   payloadObj["fields"]["customfield_10102"] = {"value": system}
   payloadObj["fields"]["customfield_10103"] = {"value": module}
   payloadObj["fields"]["customfield_10104"] = due_date
   payloadObj["fields"]["customfield_10105"] = 0
   payloadObj["fields"]["customfield_10107"] = {"type": "doc","version": 1,"content": [{"type": "paragraph","content": [{"type": "text","text": description}]}]}
-  payloadObj["fields"]["customfield_10018"] = epic_name
   payloadObj["fields"]["customfield_10300"] = ticket
   payloadObj["fields"]["issuetype"] = {"name": "Epic"}
   payloadObj["fields"]["project"] = {"key": project}
@@ -106,6 +106,7 @@ def create_story(project, name, summary, description, assignee_id, reporter_id, 
   }
   '''
 
+  '''
   payloadObj = {
     "fields": {
       "project": {
@@ -139,7 +140,21 @@ def create_story(project, name, summary, description, assignee_id, reporter_id, 
       }
     },
   }
+  '''
 
+  payloadObj = {
+    "fields": {}
+  }
+
+  payloadObj["fields"]["assignee"] = {"id": assignee_id}
+  payloadObj["fields"]["customfield_10022"] = original_estimate
+  payloadObj["fields"]["description"] = {"type": "doc","version": 1,"content": [{"type": "paragraph","content": [{"type": "text","text": description}]}]}
+  payloadObj["fields"]["issuetype"] = {"name": name}
+  payloadObj["fields"]["parent"] = {"key": parent_key}
+  payloadObj["fields"]["project"] = {"key": project}
+  payloadObj["fields"]["reporter"] = {"id": reporter_id}
+  payloadObj["fields"]["summary"] = summary  
+  
   payload = json.dumps(payloadObj)
 
   response = requests.request("POST", url, headers=headers, data=payload)
@@ -189,6 +204,7 @@ def create_task(project, name, summary, description, assignee_id, reporter_id, p
   }
   '''
 
+  '''
   payloadObj = {
     "fields": {
       "project": {
@@ -224,7 +240,21 @@ def create_task(project, name, summary, description, assignee_id, reporter_id, p
       }
     }
   }
+  '''
 
+  payloadObj = {
+    "fields": {}
+  }
+
+  payloadObj["fields"]["assignee"] = {"id": assignee_id}
+  payloadObj["fields"]["description"] = {"type": "doc","version": 1,"content": [{"type": "paragraph","content": [{"type": "text","text": description}]}]}
+  payloadObj["fields"]["issuetype"] = {"name": name}
+  payloadObj["fields"]["parent"] = {"key": parent_key}
+  payloadObj["fields"]["project"] = {"key": project}
+  payloadObj["fields"]["reporter"] = {"id": reporter_id}
+  payloadObj["fields"]["summary"] = summary
+  payloadObj["fields"]["timetracking"] = {"originalEstimate": original_estimate}
+  
   payload = json.dumps(payloadObj)
 
   response = requests.request("POST", url, headers=headers, data=payload)
